c29db56153ce4ffd64b5e8562edb7282f209ab54
Shay Palachy <shaypal5@gmail.com>
2019-04-30 19:09:17 +0300

    not testing on python 3.5

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     url='https://github.com/shaypal5/stationarizer',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=INSTALL_REQUIRES,
     extras_require={
         'test': TEST_REQUIRES + INSTALL_REQUIRES,
@@ -50,7 +50,6 @@ setuptools.setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries',
